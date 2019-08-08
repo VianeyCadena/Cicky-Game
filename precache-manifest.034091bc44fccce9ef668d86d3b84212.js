f6191de50c4eeec144392632891b8b95c792f8a3
unknown <cortikalch@hotmail.com>
2019-08-07 22:02:58 -0500

    Updates

---
+++ precache-manifest.034091bc44fccce9ef668d86d3b84212.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6441122652a4e8efdc99e14d9080162d",
+    "url": "/Cicky-Game/index.html"
+  },
+  {
+    "revision": "11359ed278c2fffff1e0",
+    "url": "/Cicky-Game/static/css/main.f46ba739.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Cicky-Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "11359ed278c2fffff1e0",
+    "url": "/Cicky-Game/static/js/main.91337842.chunk.js"
+  },
+  {
+    "revision": "9b19ab7a59ddd8a9492a",
+    "url": "/Cicky-Game/static/js/runtime~main.c8f5a4d2.js"
+  }
+]);
